764c929aa191b5b496688ffab98b9f74a4a3545b
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-07-07 21:09:47 +0300

    created and edit email templates

--- emailTemplates/registertemplate.js
+++ emailTemplates/registertemplate.js
@@ -1,11 +1,9 @@
 
 // To Do adding complete design 
-const registerTemplate = (id, code) => {
+const registerTemplate = ({ id, code }) => {
     return `<h1 style='color:blue;font-size:45px'>Register</h1> 
     <p>https://www.shop-hop.store/activate?${id}&${code}</p>
     <p>At problem visit https://www.shop-hop.store/activate and insert code: ${code}</p>`
 }
 
-module.exports = {
-    registerTemplate,
-}
+module.exports = registerTemplate;
