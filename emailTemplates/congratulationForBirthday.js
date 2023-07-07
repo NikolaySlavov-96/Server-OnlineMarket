19f18c4715118d87cb1bc6be80c56518f1ebe46c
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-07-07 21:14:13 +0300

    change text

--- emailTemplates/congratulationForBirthday.js
+++ emailTemplates/congratulationForBirthday.js
@@ -1,7 +1,7 @@
 
 // To Do adding complete design 
 const congratulationForBirthday = ({ firstName, lastName, code }) => {
-    return `<h1 style='color:blue;font-size:45px'>Happy Birthday</h1> 
+    return `<h1 style='color:blue;font-size:45px'>Today your Birthdayy</h1> 
     <p>${firstName} ${lastName} your Birthday discount code ${code}</p>
     <p>At problem visit https://www.shop-hop.store/activate and insert code: ${code}</p>`
 }
