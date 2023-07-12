8d06d41c3b2011ae90c97d796cb5a34e420406c6
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-07-12 22:36:13 +0300

    adding recall time

--- script/indexScript.js
+++ script/indexScript.js
@@ -1,12 +1,15 @@
 const expiredCode = require("./expireCodeScript");
+const { sendGiftForBirthday, congratulationForBirthday } = require("./birthdayScript");
 
 const { milisecondsOfDays } = require("../util/dates");
 const { createInterval } = require("../util/setInterval");
 
 
 function call() {
-    createInterval(milisecondsOfDays(), expiredCode);
-    // createInterval(1000, expiredCode);
+    const dayToMilisecond = milisecondsOfDays();
+    createInterval(dayToMilisecond, expiredCode);
+    createInterval(dayToMilisecond + 300000, sendGiftForBirthday);
+    createInterval(dayToMilisecond + 600000, congratulationForBirthday);
 }
 
 
