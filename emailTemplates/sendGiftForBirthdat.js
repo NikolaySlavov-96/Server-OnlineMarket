2436a47d4edbad4429351c0a8f0e28801ba674dc
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-07-12 15:46:21 +0300

    editing fields in email template

--- emailTemplates/sendGiftForBirthdat.js
+++ emailTemplates/sendGiftForBirthdat.js
@@ -1,17 +1,17 @@
 
 // To Do adding complete design and edit link after complete FrontEnd
-const sendGiftForBirthdat = ({ firstName, lastName, code }) => {
+const sendGiftForBirthdat = ({ firstName, lastName, code: { promocode, purcendDiscount} }) => {
     return `<div style="background-color:rgba(209, 209, 209, 0.2);display: inline-block;border-radius: 30px;padding: 20px;">
     <h1 style="color:rgba(0, 0, 255, 0.578);font-size:28px;">Happy Birthday</h1>
-    <p>${firstName} ${lastName} your Birthday discount code ${code}</p>
+    <p>${firstName} ${lastName} your Birthday discount code ${promocode}</p>
     <p style="display: inline-block; margin-right: 20px;">Adding your activation code from</p>
     <a style="padding:12px 18px; background-color: rgb(184, 237, 237);border-radius: 20px;"
-        href="https://www.shop-hop.store/shoping?rewardcode=${code}">Adding Code</a>
+        href="https://www.shop-hop.store/shoping?rewardcode=${promocode}">Adding Code</a>
     <p>At problem visit <span style="color:blue;font-size: 18px;">https://www.shop-hop.store/shoping</span> and
         insert code:
-        <span style="font-size: 22px;">${code}</span>
+        <span style="font-size: 22px;">${promocode}</span>
     </p>
-    <p>послучай наближаващия ви рожден ден печелите 30 % отстъпка кода може да се използва в рамките на 7 дни</p>
+    <p>послучай наближаващия ви рожден ден печелите ${purcendDiscount}% отстъпка кода може да се използва в рамките на 7 дни</p>
 </div>`
 }
 
